594bac45dc2bdbf73911b2eb92cdbebc1bcf2ae1
Viraj Surve <virajsurve710@gmail.com>
2024-07-15 00:03:20 +0530

    14 mar update

--- server/index.js
+++ server/index.js
@@ -125,6 +125,7 @@ app.post('/chat', async (req, res) => {
   }
 });
 
-app.listen(3001, '0.0.0.0', () => {
-  console.log('Server is running on port 3001');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
