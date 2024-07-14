d921f66dbb46e3391cddfaa432288aa1d0a68f5d
Viraj Surve <virajsurve710@gmail.com>
2024-07-14 17:50:20 +0530

    added vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: 'src', // Adjust the root directory as needed
-  build: {
-    outDir: 'dist', // Adjust the output directory if needed
-  },
+  plugins: [react()],
 })
